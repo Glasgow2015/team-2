88cc5b102228603af182a6b1fc9765902fd8f3b4
Christian Shtarkov <cshtarkov@gmail.com>
2015-11-07 15:50:14 +0000

    Fix models

--- server/urbanroots/adminapi/models.py
+++ server/urbanroots/adminapi/models.py
@@ -29,8 +29,8 @@ class Job(models.Model):
     created = models.DateField()
     completed = models.DateField()
     accepted = models.BooleanField()
-    latitude = models.DecimalField()
-    longitude = models.DecimalField()
+    latitude = models.DecimalField(max_digits = 10, decimal_places = 10)
+    longitude = models.DecimalField(max_digits = 10, decimal_places = 10)
     description = models.TextField()
 
     def __unicode__(self):
