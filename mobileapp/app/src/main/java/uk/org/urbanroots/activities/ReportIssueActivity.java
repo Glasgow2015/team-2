08d24df9b08537a18916ede6bdb6c8c6a12c0ecf
Kristian Sonev <krisrsd@gmail.com>
2015-11-07 21:46:21 +0000

    Issue data to server

--- mobileapp/app/src/main/java/uk/org/urbanroots/activities/ReportIssueActivity.java
+++ mobileapp/app/src/main/java/uk/org/urbanroots/activities/ReportIssueActivity.java
@@ -1,16 +1,35 @@
 package uk.org.urbanroots.activities;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,43 +37,61 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+import uk.org.urbanroots.network.Requests;
 import uk.org.urbanroots.urbanroots.R;
+import uk.org.urbanroots.util.ToolbarVisualiser;
 
 public class ReportIssueActivity extends AppCompatActivity {
 
     private EditText etTitle;
     private EditText etDescription;
+    private String[] locations;
+    private TextView tvLocation;
+    private String mLocation;
+    private RequestQueue mRequestQueue;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_issue);
+        ToolbarVisualiser.visualiseToolbar(this, "New Issue", true);
 
         etTitle = (EditText) findViewById(R.id.et_title);
         etDescription = (EditText) findViewById(R.id.et_description);
+        tvLocation = (TextView) findViewById(R.id.tv_chosen_location);
+
+        mRequestQueue = Volley.newRequestQueue(this);
+        locations = getResources().getStringArray(R.array.locations);
+        mLocation = locations[0];
     }
 
     public void sendReport(View view) {
         final String title = etTitle.getText().toString();
         final String description = etDescription.getText().toString();
+        final String location = tvLocation.getText().toString();
 
-        Log.w(LoginScreen.LOG_TAG, title);
-        Log.w(LoginScreen.LOG_TAG, description);
+        if (title.isEmpty() || description.isEmpty() || location.isEmpty()) {
+            Toast.makeText(this, "Please complete all fields", Toast.LENGTH_LONG).show();
+            return;
+        }
 
-        String url = "http://httpbin.org/post";
+        String url = Requests.getInstance().getBaseUrl() + "report/submit/2";
 
-        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
+        JSONObject jo = new JSONObject();
+        try {
+            jo.put("name", title);
+            jo.put("description", description);
+            jo.put("location", location);
+            jo.put("latitude", 0);
+            jo.put("longitude", 0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, jo,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response).getJSONObject("form");
-                            String site = jsonResponse.getString("site"),
-                                    network = jsonResponse.getString("network");
-                            Log.w(LoginScreen.LOG_TAG, "Site: "+site+"\nNetwork: "+network);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                    public void onResponse(JSONObject response) {
+                        Log.w(LoginScreen.LOG_TAG, response.toString());
                     }
                 },
                 new Response.ErrorListener() {
@@ -62,18 +99,59 @@ public class ReportIssueActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
                         error.printStackTrace();
                     }
-                }
-        ) {
+                });
+
+
+
+        mRequestQueue.add(postRequest);
+    }
+
+    public void pickLocation(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick a location");
+        builder.setItems(locations, new DialogInterface.OnClickListener() {
             @Override
-            protected Map<String, String> getParams()
-            {
-                Map<String, String> params = new HashMap<>();
-                // the POST parameters:
-                params.put("name", title);
-                params.put("description", description);
-                return params;
+            public void onClick(DialogInterface dialog, int which) {
+                tvLocation.setText(locations[which]);
+                mLocation = locations[which];
             }
-        };
-        Volley.newRequestQueue(this).add(postRequest);
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    // Redundant for now
+    private class LocationGetter implements ConnectionCallbacks, OnConnectionFailedListener {
+
+        private GoogleApiClient mGoogleApiClient;
+        private Context mContext;
+
+        LocationGetter(Context c) {
+            mContext = c;
+        }
+
+        protected synchronized void buildGoogleApiClient(Context context) {
+            mGoogleApiClient = new GoogleApiClient.Builder(mContext)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        @Override
+        public void onConnected(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+
+        }
+
+        @Override
+        public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        }
     }
 }
