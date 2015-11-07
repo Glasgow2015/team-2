1a0c82e371612e37e0e68a1ab62736c573fdc479
Christian Shtarkov <cshtarkov@gmail.com>
2015-11-07 20:11:38 +0000

    Added import json

--- server/urbanroots/adminapi/views.py
+++ server/urbanroots/adminapi/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 from adminapi.models import Job
 
