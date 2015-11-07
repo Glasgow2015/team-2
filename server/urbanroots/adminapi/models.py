f928f06c373410d119dadd772a98327488e306c5
THippner <icelore@gmail.com>
2015-11-07 15:35:26 +0000

    Some Initial models

--- server/urbanroots/adminapi/models.py
+++ server/urbanroots/adminapi/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Volunteer
+#class User_Volunteer(models.Model)
+
+
+# Area Name
+class Area(models.Model):
+    name = models.CharField(max_length=128, Unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+# Job Category
+class Category(models.Model):
+    name = models.CharField(max_length=128, Unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+# Job instance
+class Job(models.Model):
+    name = models.CharField(max_length=128)
+    created = models.DateField()
+    completed = models.DateField()
+    accepted = models.BooleanField()
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+
+# Volunteer Skills
+class Skills(models.Model):
+    name = models.CharField(max_length=128, Unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+
+
+
+
+
