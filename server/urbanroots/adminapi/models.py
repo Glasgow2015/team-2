c65f821d1b28a86bb412a90a6f7751b284966c68
THippner <icelore@gmail.com>
2015-11-07 16:29:08 +0000

    Finishied relationships

--- server/urbanroots/adminapi/models.py
+++ server/urbanroots/adminapi/models.py
@@ -14,6 +14,10 @@ class UserVolunteer(models.Model):
     # additional attributes
     phone_number = models.CharField(max_length=15)
 
+    # relationships
+    skills_list = models.ForeignKey(SkillsList, blank=True, null=True)
+    areas_list = models.ForeignKey(AreasList)
+
 
 # Urban Roots Admin
 class URAdmin(models.Model):
@@ -41,12 +45,15 @@ class Category(models.Model):
 # Job instance
 class Job(models.Model):
     name = models.CharField(max_length=128)
-    created = models.DateTimeField(auto_now_add=True)
-    completed = models.DateTimeField()
-    accepted = models.BooleanField()
+    created = models.DateTimeField(default=datetime.now, blank=True)
+    completed = models.DateTimeField(blank=True)
+    accepted = models.BooleanField(default=False)
     latitude = models.DecimalField(max_digits=10, decimal_places=10)
     longitude = models.DecimalField(max_digits=10, decimal_places=10)
     description = models.TextField()
+
+    # Relationships
+    creator = models.ForeignKey(User, blank=True, null=True)
 
     def __unicode__(self):
         return self.name
@@ -60,8 +67,23 @@ class Skill(models.Model):
         return self.name
 
 
+# Intersecting Entities to resolve Many to Many rels
+
+# List of Skills for Volunteers
+class SkillsList(models.Model):
+    volunteer = models.ForeignKey(UserVolunteer, related_name='volunteer')
+    skill = models.ForeignKey(Skill, related_name='skill')
+
+    class Meta:
+        unique_together = ('volunteer', 'skill')
 
 
+# List of areas Volunteers are interested in
+class AreasList(models.Model):
+    volunteer = models.ForeignKey(UserVolunteer, related_name='volunteer')
+    area = models.ForeignKey(Area, trlated_name='area')
 
+    class Meta:
+        unique_together = ('volunteer', 'area')
 
 
