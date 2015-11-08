614f493963e84919f24d2d9fa1d50c337407df68
Kristian Sonev <krisrsd@gmail.com>
2015-11-08 04:08:13 +0000

    Task card view and recycler view

--- mobileapp/app/src/main/java/uk/org/urbanroots/activities/LoggedInActivity.java
+++ mobileapp/app/src/main/java/uk/org/urbanroots/activities/LoggedInActivity.java
@@ -2,17 +2,36 @@ package uk.org.urbanroots.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import uk.org.urbanroots.adapters.TaskAdapter;
+import uk.org.urbanroots.models.Task;
 import uk.org.urbanroots.urbanroots.R;
 import uk.org.urbanroots.util.ToolbarVisualiser;
 
 public class LoggedInActivity extends AppCompatActivity {
 
+    private final Task [] mockTasks = {
+        new Task("description 1", "Title 2", "Location  3"),
+            new Task("description 2", "Title 3", "Location 5"),
+            new Task("description 2", "Title 3", "Location 5"),
+            new Task("description 2", "Title 3", "Location 5")
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
 
         ToolbarVisualiser.visualiseToolbar(this, "Urban Roots", false);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        RecyclerView rv = (RecyclerView)findViewById(R.id.rv_tasks);
+        rv.setLayoutManager(llm);
+        TaskAdapter ta = new TaskAdapter(new ArrayList<>(Arrays.asList(mockTasks)));
+        rv.setAdapter(ta);
     }
 }
