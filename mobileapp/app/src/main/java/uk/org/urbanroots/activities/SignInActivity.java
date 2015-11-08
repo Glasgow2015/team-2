5faba81ddb900cd2629bc525a63f07a2b88d05cd
Mike Harling <mike.harling@rocketmail.com>
2015-11-08 00:48:11 +0000

    added default login

--- mobileapp/app/src/main/java/uk/org/urbanroots/activities/SignInActivity.java
+++ mobileapp/app/src/main/java/uk/org/urbanroots/activities/SignInActivity.java
@@ -1,9 +1,11 @@
 package uk.org.urbanroots.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -22,26 +24,32 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         ToolbarVisualiser.visualiseToolbar(this, "Sign In", true);
+
+        etEmail = (EditText) findViewById(R.id.et_email);
+        etPassword = (EditText) findViewById(R.id.et_password);
     }
 
-
-
-    public void submitRegistration(View view) {
-
+    public void submitSignIn(View view) {
         final String email = etEmail.getText().toString();
         final String password = etPassword.getText().toString();
 
         Log.d(LoginScreen.LOG_TAG, email);
         Log.d(LoginScreen.LOG_TAG, password);
 
-        if (    email.isEmpty() ||
-                password.isEmpty())
-            Toast.makeText(getApplicationContext(), "Data missing", Toast.LENGTH_LONG).show();
-        else {
-            // Send data to server here
-            Toast.makeText(getApplicationContext(), "Login Successfull", Toast.LENGTH_LONG).show();
+        if(email.equals("admin") && password.equals("admin")) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Logging in...",
+                    Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
+            toast.show();
+
             Intent intent = new Intent(this, LoginScreen.class);
             startActivity(intent);
+        } else{
+            Toast toast = Toast.makeText(getApplicationContext(), "Invalid login",
+                    Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
+            toast.show();
         }
     }
-}
+    }
+
