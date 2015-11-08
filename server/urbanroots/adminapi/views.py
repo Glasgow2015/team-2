97e3742180c6ca494aba95cb31fd0330182bdbcf
THippner <icelore@gmail.com>
2015-11-08 00:25:28 +0000

    Report view functionality

--- server/urbanroots/adminapi/views.py
+++ server/urbanroots/adminapi/views.py
@@ -170,16 +170,10 @@ def report(request, reportid):
     context_dict = {}
 
     # fetch report data
-    report = Job.objects.get(id=reportid)
-    context_dict['report_name'] = report.name
-    context_dict['report_creation_date'] = report.created
-    context_dict['report_completion_date'] = report.completed
-    context_dict['report_accepted'] = report.accepted
-    context_dict['report_latitude'] = report.latitude
-    context_dict['report_longitude'] = report.longitude
-    context_dict['report_description'] = report.description
+    context_dict['report'] = Job.objects.get(id=reportid)
+    context_dict['volunteers'] = UserVolunteer.objects.all()
 
-    return render(request, '', context_dict)
+    return render(request, 'report.html', context_dict)
 
 @csrf_exempt
 def job(request, jobid):
