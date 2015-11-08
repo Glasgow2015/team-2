47026a3ec12cdfef718be852441bce602c51db2f
Mike Harling <mike.harling@rocketmail.com>
2015-11-08 04:45:24 +0000

    added toasts to a couple views

--- mobileapp/app/src/main/java/uk/org/urbanroots/activities/LoggedInActivity.java
+++ mobileapp/app/src/main/java/uk/org/urbanroots/activities/LoggedInActivity.java
@@ -2,11 +2,21 @@ package uk.org.urbanroots.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+
+import org.json.JSONObject;
 
 import uk.org.urbanroots.urbanroots.R;
 import uk.org.urbanroots.util.ToolbarVisualiser;
 
 public class LoggedInActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +24,7 @@ public class LoggedInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_logged_in);
 
         ToolbarVisualiser.visualiseToolbar(this, "Urban Roots", false);
+
+
     }
 }
